bbdf0a6a02af2e920b3d68b8bc0ea60a948699a0
Rick Graham <rick@raddstudio.com>
2014-01-30 17:25:05 -0500

    implementation js of the dice mvc

---
+++ js/rolla.js
@@ -0,0 +1,52 @@
+// pass the models into the controller
+rs.roller = rs.controller.init([
+  {id:'die',el:'#die',
+    u: function(){
+      this.value=Math.floor(Math.random() * 6);
+      return this;
+    },
+    v: function(m){
+      var v=m.value ? m.value : 6;
+      var nodes=this.el.getElementsByTagName('circle');
+      for (var i = 0; i < nodes.length; i++){
+        var n=nodes[i];
+        var c=n.getAttribute('class');
+        (c.indexOf(v) >= 0) ? n.removeAttribute('display') : n.setAttribute('display','none');
+      }
+      
+      m.text = v;
+      return this.render(m);
+    }
+  },
+  {id:'dice', el:'#dice',
+    u: function(){
+      this.die=rs.roller.get('die');
+      this.dice=[];
+      for (var i=0; i<rs.dice; i++){
+        var d=Object.create(this.die);
+        d.y=100;
+        d.x=i*120;
+
+        this.dice.push(d);
+      }
+      return this;
+    },
+    v: function(m){
+      for (var i=0; i<m.dice.length; i++){
+        var el=document.createElementNS(svgns,'g');
+        el.setAttribute('class','die');
+        
+        //set the position of the new die
+        var d=m.dice[i];
+        d.v.o.set_transform(el,d);
+        
+        //copy the content of the dice to the el
+        var copy=d.v.o.update(d.m.o.update());
+        for (var j=0; j<copy.childNodes.length; j++){
+          el.appendChild(copy.childNodes[j].cloneNode());
+        }
+        this.el.appendChild(el);
+      }
+    }
+  }
+]);
